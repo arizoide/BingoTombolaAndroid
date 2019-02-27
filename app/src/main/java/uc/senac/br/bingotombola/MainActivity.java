b6f5224d33d18476e7678b3ded857caf3d350d34
Ari Thomazini <ari.thomazini@gmail.com>
2019-02-27 00:15:32 -0300

    Initial commit

---
+++ app/src/main/java/uc/senac/br/bingotombola/MainActivity.java
@@ -0,0 +1,13 @@
+package uc.senac.br.bingotombola;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
